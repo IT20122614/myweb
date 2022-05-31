82ef1689585717116ea3aad6b5a966929face97e
Kavindu Madushan <kavindu@tsdclanka.com>
2022-05-31 22:02:04 +0530

    heroku

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const app = express();
 require("./startUp/db")();
 require("./startUp/config")();
 require("./startUp/routes")(app);
+
 app.get("/", (req, res)=>{
   res.send("hello world");
 })
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
-  console.log(`Listning on Port : ${port} `);
+  console.log(`Listning on Port : ${port} `, this.address().port, app.settings.env);
 });
 
 module.exports = server;
