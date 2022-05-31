c8b9dbb6169c9045cc24be5a5331f739212dcb96
Kavindu Madushan <kavindu@tsdclanka.com>
2022-05-31 22:08:06 +0530

    herokus

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res)=>{
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
-  console.log(`Listning on Port : ${port} `, this.address().port, app.settings.env);
+  console.log(`Listning on Port : ${port} `, app.settings.env);
 });
 
 module.exports = server;
